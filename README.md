3f2739fc28305fe413dd82c9bfbe3b7a305a325a
zhuqiangqiang <zhuqiangqiang@host.docker.internal>
2020-01-07 11:23:02 +0800

    修改错误

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # activemq
 ### 错误 ：Cannot create SystemUsage
+修改：vm://地址 改为 tcp://地址
+简单的测试demo
+
--- src/main/java/com/onepage/activemq/service/Impl/DemoServiceImpl.java
+++ src/main/java/com/onepage/activemq/service/Impl/DemoServiceImpl.java
@@ -1,10 +1,9 @@
 package com.onepage.activemq.service.Impl;
 
-import javax.annotation.Resource;
 import javax.jms.Connection;
 import javax.jms.DeliveryMode;
 import javax.jms.Destination;
-import javax.jms.ExceptionListener;
+
 import javax.jms.Message;
 import javax.jms.MessageConsumer;
 import javax.jms.MessageProducer;
@@ -24,99 +23,61 @@ public class DemoServiceImpl  implements DemoService {
 	public void doIt() {
 //		Thread thread=new Thread();
 		
-		Thread tt=new Thread(()-> {
+//		Thread tt=new Thread(()-> {
 			
 			System.out.println("hashhhhh");
-			ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("vm://localhost");
+			ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://localhost:61616");
 			System.out.println("conn success");
 			try(Connection connection = connectionFactory.createConnection()){
+				System.out.println("--------------開始了");
 				connection.start();
-				try {
-					Thread.sleep(5000);
-				} catch (InterruptedException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
 				System.out.println("-------------start producer---------------->>>>>>>>>");
 //				connection.setExceptionListener((ExceptionListener) this);
 				
 				 Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-				 
+				 //创建一个目标
 				 Destination destination = session.createQueue("TEST.FOO");
 				 System.out.println("-------------here producer---------------->>>>>>>>>");
-				 MessageConsumer consumer = session.createConsumer(destination);
-				 Message message = consumer.receive(1000);
-				  if (message instanceof TextMessage) {
-	                  TextMessage textMessage = (TextMessage) message;
-	                  String text = textMessage.getText();
-	                  System.out.println("Received: " + text);
-	              } else {
-	                  System.out.println("Received: " + message);
-	              }
+				 //创建一个生产者
+				 MessageProducer producer = session.createProducer(destination);
+				 //设置消息模式  非持久
+				 producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
+				 //创建一个消息
+				 String text = "Hello world! From: " + Thread.currentThread().getName() + " : " + this.hashCode();
+				 TextMessage message = session.createTextMessage(text);
+	                //發送消息
+				 producer.send(message);
+	              
 			}catch (Exception e) {
 				System.out.println("error------------->"+e.getMessage());
 			}
-			
-		});
-		tt.setDaemon(false);
-		tt.start();
-//	System.out.println("hashhhhh");
-//		ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory("vm://localhost");
-//		System.out.println("conn success");
-//		try(Connection connection = connectionFactory.createConnection()){
-//			connection.start();
-//			try {
-//				Thread.sleep(5000);
-//			} catch (InterruptedException e1) {
-//				// TODO Auto-generated catch block
-//				e1.printStackTrace();
-//			}
-//			System.out.println("-------------start producer---------------->>>>>>>>>");
-////			connection.setExceptionListener((ExceptionListener) this);
-//			
-//			 Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-//			 
-//			 Destination destination = session.createQueue("TEST.FOO");
-//			 System.out.println("-------------here producer---------------->>>>>>>>>");
-//			 MessageConsumer consumer = session.createConsumer(destination);
-//			 Message message = consumer.receive(1000);
-//			  if (message instanceof TextMessage) {
-//                  TextMessage textMessage = (TextMessage) message;
-//                  String text = textMessage.getText();
-//                  System.out.println("Received: " + text);
-//              } else {
-//                  System.out.println("Received: " + message);
-//              }
-//		}catch (Exception e) {
-//			System.out.println("error------------->"+e.getMessage());
-//		}
-		try {
-			Thread.sleep(5000);
-		} catch (InterruptedException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}
-		ActiveMQConnectionFactory factory1 =new ActiveMQConnectionFactory();
+			//接受消息
+//		ActiveMQConnectionFactory factory1 =new ActiveMQConnectionFactory("tcp://localhost:61616");
 		System.out.println("-------------here consumer---------------->>>>>>>>>");
-		try(Connection connection=factory1.createConnection()){
+		try(Connection connection=connectionFactory.createConnection()){
+			//生產者鏈接成功
+			System.out.println("生產者鏈接成功");
 			connection.start();
 			//创建会话
 		Session session=connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
 		System.out.println("-------------here consumer---------------->>>>>>>>>");
+		//創建一個目標
 		Destination destination= session.createQueue("TEST.FOO");
-		MessageProducer producer =session.createProducer(destination);
+		//創建一個消費者
+		 MessageConsumer consumer = session.createConsumer(destination);
 		System.out.println("-------------here consumer---------------->>>>>>>>>");
-		producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);
-		//创建消息
-		System.out.println("打印消息-----------》");
-		 String text = "Hello world! From: " + Thread.currentThread().getName() + " : " + this.hashCode();
-         TextMessage message = session.createTextMessage(text);
-         //发送消息
-         producer.send(message);
+		//等待消息
+		Message message = consumer.receive(1000);
+		 if (message instanceof TextMessage) {
+             TextMessage textMessage = (TextMessage) message;
+             String text = textMessage.getText();
+             System.out.println("Received: " + text);
+         } else {
+             System.out.println("Received: " + message);
+         }
 		}catch (Exception e) {
 			System.out.println(e);
 		}
-		
 		
 		
 	}
